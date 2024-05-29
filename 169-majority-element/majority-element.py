e378519f8f812d4bd7ceb7d5c59440bca4d2cd5e
Myles Miller <163357265+ozemoya@users.noreply.github.com>
2024-05-28 23:01:20 -0400

    Time: 184 ms (12.12%) | Memory: 18 MB (76.20%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,12 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:  
+        #Make a Hash
         freq = defaultdict(int)
-        
+
+        #make for loop evaluating 
         for num in nums:
             if num in freq.keys():
-                freq[num] += 1 
-            else: 
-                freq[num] = 1
+                freq[num]+= 1
+            else:
+                freq[num]=1
         
-            if freq[num] > (len(nums)/2):
+        #if you appear more than two times return val
+            if freq[num] > (len(nums) / 2):
                 return num
