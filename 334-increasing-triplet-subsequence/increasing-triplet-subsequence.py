74b92bf58e667b8804fd69a7ce357f87bf94eefa
Myles Miller <163357265+ozemoya@users.noreply.github.com>
2024-06-02 12:35:43 -0400

    Time: 789 ms (57.25%) | Memory: 32.5 MB (68.47%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
@@ -1,7 +1,7 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
+        #TC: [1,2,3,4,5]
         first = second = float('inf')
-        
         for num in nums:
             if num <= first:
                 first = num
@@ -9,11 +9,4 @@ class Solution:
                 second = num
             else:
                 return True
-                
         return False
-
-# Test the function with the given test case
-solution = Solution()
-print(solution.increasingTriplet([1, 2, 3, 4, 5]))  # Output should be True
-print(solution.increasingTriplet([5, 4, 3, 2, 1]))  # Output should be False
-print(solution.increasingTriplet([2, 1, 5, 0, 4, 6]))  # Output should be True
