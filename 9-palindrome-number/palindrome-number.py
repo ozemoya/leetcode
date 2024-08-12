9424f2146ca8eb72db80e2e2a63eb12544111403
Myles Miller <163357265+ozemoya@users.noreply.github.com>
2024-08-12 17:54:42 -0400

    Time: 52 ms (65.31%) | Memory: 16.6 MB (37.08%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,6 +1,15 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        reversed = str(x)[::-1]
-        if reversed == str(x):
-            return True
-        return False
+        if x < 0:
+            return False
+        #Convert x to string and then use two Pointer method first and last
+        str_x = str(x)
+        first = 0
+        last = len(str_x)-1
+        while first <= last:
+            if str_x[first] != str_x[last]:
+                return False
+            first +=1
+            last -=1
+        return True
+    
